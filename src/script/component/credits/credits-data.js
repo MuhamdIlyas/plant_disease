8f0aa6640b06d9345095e2e419bfbae3509c7066
muhamadilyas17 <lyas170100@gmail.com>
2022-12-24 20:34:11 +0700

    Skrips Plant Disease

---
+++ src/script/component/credits/credits-data.js
@@ -0,0 +1,49 @@
+const data = [
+	{
+		title: 'Datasets 1',
+		link : 'https://www.kaggle.com/datasets/minhhuy2810/rice-diseases-image-dataset',
+		text : 'Dataset Padi ',
+	},
+	{
+		title: 'Dataset 2',
+		link : 'https://www.kaggle.com/datasets/vipoooool/new-plant-diseases-dataset',
+		text : 'Dataset Jagung dan Tomat',
+	},
+	{
+		title:'Dataset 3',
+		link : 'https://www.kaggle.com/datasets/kareem3egm/cucumber-plant-diseases-dataset',
+		text : 'Dataset Mentimun',
+	},
+	{
+		title: 'Palette',
+		link: 'https://coolors.co/264653-2a9d8f-e76f51-ddd8b8-ffffff',
+		text: 'Generate from Coolors',
+	},
+	{
+		title: 'CSS Framework',
+		link: 'https://getbootstrap.com/',
+		text: 'Bootstrap 5.1.3',
+	},
+	{
+		title: 'Icon Toolkit',
+		link: 'https://fontawesome.com',
+		text: 'Font Awesome 5.15.4',
+	},
+	{
+		title: 'Gambar 1',
+		link: '#tim-pengembang',
+		text: '12 Types of Trash by Tim Pengembang',
+	},
+	{
+		title: 'Gambar 2',
+		link: 'https://www.freepik.com/free-vector/programmer-working-flat-style_4911013.htm#page=1&query=developer&position=0&from_view=search',
+		text: 'Programmer working in flat style Free Vector Freepick by @pikisuperstar',
+	},
+	{
+		title: 'Hosting Platform',
+		link: 'https://vercel.com/',
+		text: 'Vercel',
+	},
+];
+
+export default data;
