cf0779dc4eb903afae7ffa89337a7f74180537ab
muhamadilyas17 <lyas170100@gmail.com>
2023-02-14 07:50:51 +0700

    Update Information

--- src/script/component/jumbotron/jumbotron-data.js
+++ src/script/component/jumbotron/jumbotron-data.js
@@ -1,7 +1,7 @@
 const data = [
 	{
 		title: 'Plant Disease',
-		description: 'Mengklasifikasikan 4 jenis penyakit tanaman yang terdiri dari Jagung, Mentimun, Tomat dan padi melalui gambar menggunakan teknik Machine Learning dengan Algoritma Convolutional Neural Network, agar dapat mendiagnosa jenis penyakit pada tanaman.',
+		description: 'Mengklasifikasikan 4 jenis penyakit tanaman yang terdiri dari Jagung, Mentimun, Tomat dan padi melalui gambar menggunakan teknik Deep Learning dengan Algoritma Convolutional Neural Network, agar dapat mendiagnosa jenis penyakit pada tanaman.',
 		image: 'https://images.unsplash.com/photo-1421789665209-c9b2a435e3dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80',
 		alt: 'Jenis-Jenis Tanaman'
 	},
